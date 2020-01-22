f21506623bc7fc6fb2677726098d7f1a685c37e5
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-22 08:17:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,9 +14,7 @@ def directors_totals(nds)
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
   
-  total = 0 
-  row = 0 
-  column = 0
+  
   while row < directors_database[0][:movies].length do
     result[directors_database][:name] = directors_database[0][row][:worldwide_gross]
   end
